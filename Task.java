01a12cb2b68cc6588b652171e3efa668098fba6e
Manasa Rajendran <manasa20405@gmail.com>
2026-02-04 21:09:10 +0530

    updated a new feature attempt

--- Task.java
+++ Task.java
@@ -4,22 +4,24 @@ public class Task {
         Scanner sc=new Scanner(System.in);
         int r=(int)(Math.random()*10)+1;
         boolean guess=false;
-        for(int i=0;i<5;i++){
+        int count =1;
+        for(int i=0;i<3;i++){
             System.out.println("Enter the number you guess(from 1 to 10)");
             int n=sc.nextInt();
-            if(rnd(n,r)){
+            if(rnd(n,r,count)){
                 guess=true;
                 break;
             }
+            count++;
         }
         if(!guess){
             System.out.println("Oops! better luck next time \nThe number was "+r);
         }
         sc.close();
     }
-    public static boolean rnd(int n,int r){
+    public static boolean rnd(int n,int r,int c){
         if( r== n){
-            System.out.println("You guessed right "+n);
+            System.out.println("You guessed right at attempt "+c+" ,its indeed "+n+"!");
             return true;
         }
         else if(r<n){
